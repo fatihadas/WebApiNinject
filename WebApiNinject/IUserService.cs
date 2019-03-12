76aa7f2c8a5068d8fa7657f6b2a33c56f98abf8e
Fatih ADAş <fatihadas93@gmail.com>
2019-03-12 21:47:09 +0300

    Add project files.

---
+++ WebApiNinject/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiNinject
+{
+    public interface IUserService
+    {
+        List<string> GetUserFullNames();
+    }
+}
---
+++ WebApiNinject/UserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiNinject
+{
+    public class UserService : IUserService
+    {
+        public List<string> GetUserFullNames()
+        {
+            return new List<string> { "Fatih ADAŞ", "Mehmet Yilmaz", "Ahmet Coşan", "Kadir Dönmez", "Akif Sönmez" };
+        }
+    }
+}
